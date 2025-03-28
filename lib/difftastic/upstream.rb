84b720b8e37160dbfa60b3878848271819bb5379
Arjun Singh <arjun810@gmail.com>
2025-03-27 21:38:08 -0700

    Include aarch64-linux in platform list

--- lib/difftastic/upstream.rb
+++ lib/difftastic/upstream.rb
@@ -7,6 +7,7 @@ module Difftastic
 		NATIVE_PLATFORMS = {
 			"arm64-darwin" => "difft-aarch64-apple-darwin.tar.gz",
 			"arm64-linux" => "difft-aarch64-unknown-linux-gnu.tar.gz",
+			"aarch64-linux" => "difft-aarch64-unknown-linux-gnu.tar.gz",
 			"x86_64-darwin" => "difft-x86_64-apple-darwin.tar.gz",
 			"x86_64-linux" => "difft-x86_64-unknown-linux-gnu.tar.gz",
 		}.freeze
